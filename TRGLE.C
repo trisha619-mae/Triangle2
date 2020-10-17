f416fa49a24e693212a3d91712522e9e7ae1030b
Trisha Mae V. Apeleña <72650858+trisha619-mae@users.noreply.github.com>
2020-10-17 19:05:45 +0800

    triangle

---
+++ TRGLE.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+int i, j, k;
+  clrscr();
+
+
+for(i = 1; i < 24; i++)
+  {
+for(j = 24; j >= i; j--)
+ {
+   printf(" ");
+   }
+for(k = 1; k <= i; k++)
+  printf("*");
+   printf("\n");
+
+}
+
+getch();
+}
